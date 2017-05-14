96ed923a41cfd5935a92802a03e3932bfd1d0713
MaDHoma <madhoma@proglife.com.ua>
2017-05-14 15:17:54 +0300

    a

--- app/settings.py
+++ app/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 't28j5aav#a68iz9!83415x&2_42bi7=(@k*6#vtgmgj58*2&9)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['madhoma.pythonanywhere.com',]
+ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['madhoma.pythonanywhere.com',]
 
 
 # Application definition
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'templates',
+            os.path.join(BASE_DIR, 'templates'),
             ],
         'APP_DIRS': True,
         'OPTIONS': {
